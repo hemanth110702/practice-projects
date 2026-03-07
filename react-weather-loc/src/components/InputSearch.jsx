505857f3761dd3bb7dd9453bf0035b9729d21c42
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2026-03-07 12:16:12 +0530

    feat: migrated react-weather-loc project

---
+++ react-weather-loc/src/components/InputSearch.jsx
@@ -0,0 +1,54 @@
+const app_id = import.meta.env.VITE_API_APP_ID;
+
+const InputSearch = ({ setLocation, inputRef, setLocData }) => {
+  const handleInputSearch = async (e) => {
+    if (e.target.value === "") {
+      dropdownMenu.innerHTML = "";
+    }
+    if (e.target.value != "") {
+      const search_response = await fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${e.target.value}&limit=5&appid=${app_id}`
+      );
+      const searchData = await search_response.json();
+      console.log(searchData);
+      dropdownMenu.innerHTML = "";
+      if (searchData.length === 0) {
+        const noDataItem = document.createElement("div");
+        noDataItem.classList.add("dropdown-item");
+        noDataItem.textContent = "No data available";
+        dropdownMenu.appendChild(noDataItem);
+      } else {
+        searchData.forEach((loc) => {
+          const dropDownItem = document.createElement("div");
+          dropDownItem.classList.add("dropdown-item");
+          dropDownItem.textContent = `${loc.name} ,${loc.country}`;
+          dropDownItem.addEventListener("click", () => {
+            document.querySelector(
+              ".dropdown-input"
+            ).value = `${loc.name}, ${loc.country}`;
+            e.target.value = `${loc.name}, ${loc.country}`;
+            dropdownMenu.innerHTML = "";
+            const searchLoc = { name: loc.name, country: loc.country };
+            setLocData((prevData)=> ({...prevData, temperature: "", found: true}))
+            setLocation((prevLoc) => ({ ...prevLoc, ...searchLoc }));
+          });
+          dropdownMenu.appendChild(dropDownItem);
+        });
+      }
+    }
+  };
+
+  return (
+      <>
+        <input
+          type="text"
+          className="dropdown-input input-box"
+          placeholder="Type to search locations"
+          onChange={handleInputSearch}
+          ref={inputRef}
+        />
+      </>
+  );
+};
+
+export default InputSearch;
